6c9faaa214ffbcf8b75a7e6966b934a0a3f2a9be
mariannelena <73900073+mariannelena@users.noreply.github.com>
2022-11-01 15:44:10 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,36 @@
+let P1 = 0
+let P2 = 0
+let Var = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . # . # .
-        # # # # #
-        # # # # #
-        . # # # .
-        . . # . .
-        `)
-    basic.pause(1000)
-    basic.showString("Hej allihopa ")
+    P1 += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    P2 = 0
+    P1 = 0
+})
+input.onButtonPressed(Button.B, function () {
+    P2 += 1
 })
 input.onGesture(Gesture.Shake, function () {
-    basic.showIcon(IconNames.Ghost)
-    music.playMelody("C5 B G C C A B C5 ", 120)
+    Var = randint(1, 3)
+    if (Var == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (Var == 2) {
+        basic.showIcon(IconNames.Scissors)
+    } else if (Var == 3) {
+        basic.showIcon(IconNames.Square)
+    } else {
+    	
+    }
+    basic.pause(2000)
+    basic.clearScreen()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString("P1")
+    basic.showString("" + (P1))
+    basic.pause(127)
+    basic.showString("P2")
+    basic.showString("" + (P2))
+    basic.pause(100)
     basic.clearScreen()
 })
